608fb3f74dec4afd0b1a05b6ec24330fd73095db
Filip (akaBlur) Vukoja <vukojaf@yahoo.de>
2024-02-25 13:51:03 +0100

    Unified initialization; Module requirements fixed

--- scripts/preinit.sh
+++ scripts/preinit.sh
@@ -29,3 +29,40 @@ if [ ! -e "$MOD_TUT_FILE" ];then
     cp "/defaults/modules/simple_time.py" "$MOD_TUT_FILE"
     chmod 644 "$MOD_TUT_FILE"
 fi
+
+# create empty client secret file
+CLIENTDEV="/sec/clients.dev"
+SECDIR="/sec"
+LOGDIR="/log"
+
+if [ ! -d "$LOGDIR" ];then
+    mkdir -m 777 "$LOGDIR"
+fi
+
+chmod -R 777 "$LOGDIR"
+
+if [ ! -d "$SECDIR" ];then
+    mkdir -m 755 "$SECDIR"
+fi
+
+# keys inside sec should always be readable by the server
+chmod -R 644 "$SECDIR"
+# needed for cd access
+chmod 755 "$SECDIR"
+
+if [ ! -e "$CLIENTDEV" ];then
+    touch "$CLIENTDEV"
+    chmod 644 "$CLIENTDEV"
+fi
+
+# check and create server keyfiles
+if [ ! -e "/sec/server.key" -a ! -e "/sec/server.pub" ];then
+    python3 /app/util/gen_key.py pair /sec/server
+    chmod 644 /sec/server.key
+    chmod 644 /sec/server.pub
+fi
+
+# install module requirements if supplied
+if [ -s "$MOD_REQ_FILE" ];then
+    pip install -r "$MOD_REQ_FILE"
+fi
